1da0325e4a6d7676b58e925ac51f409545bb3384
Ian Ellis <ie31784@airtricity.com>
2017-01-13 17:33:17 +0000

    progress as far more on components

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -1,11 +1,20 @@
+//this is how we import external modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
+//this is how we import internal modules
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 
+//this is how we declare which modules we would like to use from the above imported modules
 @NgModule({
-  imports: [ BrowserModule ],
+  //imports is used for external/3rd party directives, components and pipes
+  imports: [ 
+    BrowserModule,
+    FormsModule
+  ],
+  //declarations is used for our directives, components and pipes
   declarations: [
     AppComponent, 
     ProductListComponent
