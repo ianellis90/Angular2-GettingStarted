7a0c5fa29b49a792e9b8f74d69872c910fd146a1
Ian Ellis <ie31784@airtricity.com>
2017-01-16 14:01:58 +0000

    Halfway through HTTP Module

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -2,22 +2,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 //this is how we import internal modules
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ProductFilterPipe } from './products/product-filter.pipe';
+import { StarComponent } from './shared/star.component';
 
 //this is how we declare which modules we would like to use from the above imported modules
 @NgModule({
   //imports is used for external/3rd party directives, components and pipes
   imports: [ 
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   //declarations is used for our directives, components and pipes
   declarations: [
     AppComponent, 
-    ProductListComponent
+    ProductListComponent,
+    ProductFilterPipe,
+    StarComponent
   ],
   bootstrap: [ AppComponent ]
 })
